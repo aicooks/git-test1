f95f1bf1f762136c6050e02aa7a8a0a3949e528c
aicooks <94235253+aicooks@users.noreply.github.com>
2022-07-26 18:35:25 +0800

    github update

--- src/main/java/li/git/GitTest.java
+++ src/main/java/li/git/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("hello git4");
         System.out.println("ssh***********************");
         System.out.println("pull***********************");
+         System.out.println("github update***********************");
 
     }
 }
